8669ffa3c78c3151f491b5e613cee49512c75bd1
KONG1034 <kyh93054@naver.com>
2023-06-09 23:00:14 +0900

    axios&my-json-server 설치 및 데이터 가져오기 적용

--- src/components/Contents/Todo/List.tsx
+++ src/components/Contents/Todo/List.tsx
@@ -1,9 +1,21 @@
-import { useContext } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { listContext } from "../../../context/listContext";
 import { TodoLi } from "./TodoLi";
+import axios from 'axios';
 
 export const List = () => {
     const {state, dispatch} = useContext(listContext);
+
+    useLayoutEffect(() => {
+        axios.get('https://my-json-server.typicode.com/kong1034/db/list')
+        .then(res => {
+            state.todos = res.data;
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    })
+
     return <ul>
             {
                 
--- src/context/listContext.ts
+++ src/context/listContext.ts
@@ -1,7 +1,7 @@
 import { Dispatch, createContext } from "react";
 import { ITodo } from "../interface/ITodo";
-import { ITodoContext } from "../interface/ITodoContext";
+import { TodoContext } from "../interface/TodoContext";
 import { actions } from "../actions/actions";
 
 //context를 처음으로 만들고 초기값을 정의 
-export const listContext = createContext({} as ITodoContext)
+export const listContext = createContext({} as TodoContext)
